30e844ccd98202ba18a6c018ea24021f083d7425
Miklos Maroti <mmaroti@gmail.com>
2017-04-17 20:10:19 -0500

    removed a lot of unnecessary/obsolete code (there are some more)

--- src/org/haskell/data/Func.java
+++
@@ -1,9 +0,0 @@
-/**
- *	Copyright (C) Miklos Maroti, 2015
- */
-
-package org.haskell.data;
-
-public abstract class Func<ARG, RET> {
-	public abstract RET apply(ARG arg);
-}
--- src/org/haskell/typeclass/Callable.java
+++
@@ -1,9 +0,0 @@
-/**
- *	Copyright (C) Miklos Maroti, 2015
- */
-
-package org.haskell.typeclass;
-
-public abstract class Callable<FUNC, ARG, RET> {
-	public abstract RET call(FUNC func, ARG arg);
-}
